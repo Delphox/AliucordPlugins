bbd2908494a14faa175efd8490128b11f6fa7195
Delphox <smugleaf@live.com>
2022-02-21 17:44:14 -0300

    Delete plugins/LayoutController directory

--- plugins/LayoutController/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-description = "Lets you control the discord mobile layout."
-version = "1.0.5"
-
-aliucord {
-    author("zt", 289556910426816513L)
-    author("Animal", 118437263754395652L)
-    author("Diamond", 295190422244950017L)
-}
